4bf015af7f721cf38eb5c1a9478e896efb0ecd57
Akash Khan <akashcc48@gmail.com>
2022-09-11 16:47:21 +0600

    some file changes

--- js/custom.js
+++ js/custom.js
@@ -9,29 +9,21 @@ const searchFood = async () => {
     //Clear Input Value Or Previous Result
     searchFeild.value = '';
     searchResult.textContent = '';
+    //Load MealDB Data By API
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayResult(data.meals)
 
-    if (searchText == '') {
-        alert('You have not searched anything')
-    }
-    else {
-        //Load MealDB Data By API
-        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
-        const res = await fetch(url);
-        const data = await res.json();
-        displayResult(data.meals)
-    }
 }
+
+searchFood('fish');
 // Display MealDB Data
 const displayResult = async meals => {
-    console.log(meals.length);
-    if (meals == 0) {
-        alert('This Food Not Available In Our Resturant')
-    }
-    else {
-        meals.forEach(meal => {
-            const div = document.createElement('div');
-            div.classList.add('col');
-            div.innerHTML = `
+    meals.forEach(meal => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
             
             <div class="col">
                 <div class="card">
@@ -43,9 +35,9 @@ const displayResult = async meals => {
                 </div>
             </div>
             `
-            searchResult.appendChild(div)
-        })
-    }
+        searchResult.appendChild(div)
+    })
+
 
 }
 
